bff1d013d6e1518148bbb6fb98f9784ba3a2992f
Gilmore Davidson <gilmoreorless@gmail.com>
2011-11-10 08:03:29 +1100

    First draft of presentation content using Deck.js

---
+++ slides.js
@@ -0,0 +1,14 @@
+/**
+ * FEATURES:
+ * 
+ * - Slide can be full page or part of another page
+ * - Slide content can animate in (with different easing), fade in, or instantly display
+ * - Hash-based nav links are optional
+ */
+
+// Yet Another Slides Implementation
+var yasi = (function (window, document, undefined) {
+	
+	
+	
+})(this, this.document);
